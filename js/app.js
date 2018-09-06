8abc10602bb4e7d4b17a85c6f303657c7e127263
self-unit <self_unit@hotmail.com>
2018-09-06 16:14:10 +0100

    final lab solution

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+
+
+
 })
+
+const handleFormSubmit = function(event) {
+  event.preventDefault();
+
+  const newTitle = document.createElement('p');
+  newTitle.textContent = `Title: ${event.target.title.value}`;
+  newTitle.classList.add('p1');
+
+  const newAuthor = document.createElement('p');
+  newAuthor.textContent = `Author: ${event.target.author.value}`;
+  newAuthor.classList.add('p2');
+
+  const newCategory = document.createElement('p');
+  newCategory.textContent = `Category: ${event.target.category.value}`;
+  newCategory.classList.add('p3');
+
+  const newDivItem = document.createElement('div');
+  newDivItem.appendChild(newTitle);
+  newDivItem.appendChild(newAuthor);
+  newDivItem.appendChild(newCategory);
+
+  const readingList = document.querySelector('#reading-list');
+  readingList.appendChild(newDivItem);
+
+  event.target.reset();
+}
